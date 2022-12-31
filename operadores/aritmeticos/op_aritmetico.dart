0469c6cd42b043d51816d1bf699e872c7acd6f74
Paysandu <ams.protazio@gmail.com>
2022-12-31 18:24:06 -0300

    Adicionando operadores aritméticos

---
+++ operadores/aritmeticos/op_aritmetico.dart
@@ -0,0 +1,10 @@
+void main() {
+  print(40 + 2); // Adição
+  print('40' + '2'); // Concatenação
+  print(50 - 8); // Subtração
+  print(-(-42)); // Inverte o sinal da operação
+  print(6 * 7); // Multiplicação
+  print(11 / 2); // Divisão
+  print(11 ~/ 2); // Retorno da parte inteira
+  print(11 % 2); // Resto da divisão
+}
