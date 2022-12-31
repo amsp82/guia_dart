a491eca39caf725183270b7195ec5eaa1c6db895
Paysandu <ams.protazio@gmail.com>
2022-12-31 20:56:55 -0300

    Operadores de atribuição

---
+++ operadores/atribuicao/op_atribuicao1.dart
@@ -0,0 +1,22 @@
+void main() {
+  num a = 4;
+  print(a);
+
+  a += 5;
+  print(a);
+
+  a -= 4;
+  print(a);
+
+  a %= 2;
+  print(a);
+
+  a *= 11;
+  print(a);
+
+  a /= 5;
+  print(a);
+
+  a ~/= 2;
+  print(a);
+}
---
+++ operadores/atribuicao/op_atribuicao2.dart
@@ -0,0 +1,22 @@
+void main() {
+  int b = 1;
+  print(b);
+
+  b <<= 4;
+  print(b);
+
+  b |= 42;
+  print(b);
+
+  b &= 60;
+  print(b);
+
+  b ^= 42;
+  print(b);
+
+  b >>= 1;
+  print(b);
+
+  b >>>= 1;
+  print(b);
+}
